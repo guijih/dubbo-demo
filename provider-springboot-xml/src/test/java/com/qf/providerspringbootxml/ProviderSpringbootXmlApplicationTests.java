c56cbd4a85556f3b34564f4a7eb1f19f324e9110
guijih <18300613195@163.com>
2020-02-12 11:54:43 +0800

    提交

---
+++ provider-springboot-xml/src/test/java/com/qf/providerspringbootxml/ProviderSpringbootXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.providerspringbootxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderSpringbootXmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
