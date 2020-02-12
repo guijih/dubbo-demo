3d733aca756b2cce21e2c80dde075b9c6ba92eca
guijih <18300613195@163.com>
2020-02-12 16:38:24 +0800

    提交代码 springboot+dubbo+annotation方式

--- consumer-springboot-annotation/src/main/java/com/qf/consumerspringbootannotation/ConsumerSpringbootAnnotationApplication.java
+++ consumer-springboot-annotation/src/main/java/com/qf/consumerspringbootannotation/ConsumerSpringbootAnnotationApplication.java
@@ -1,9 +1,17 @@
 package com.qf.consumerspringbootannotation;
 
+import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**@Description springboot+dubbo整合
+ * @ClassName ConsumerSpringbootAnnotationApplication
+ * @author wangwei
+ * @date 2020/2/12 0012 15:04
+ */
 @SpringBootApplication
+@EnableDubbo
+@DubboComponentScan()
 public class ConsumerSpringbootAnnotationApplication {
 
     public static void main(String[] args) {
--- consumer-springboot-annotation/src/test/java/com/qf/consumerspringbootannotation/ConsumerSpringbootAnnotationApplicationTests.java
+++ consumer-springboot-annotation/src/test/java/com/qf/consumerspringbootannotation/ConsumerSpringbootAnnotationApplicationTests.java
@@ -1,13 +1,19 @@
 package com.qf.consumerspringbootannotation;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.qf.service.IUservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ConsumerSpringbootAnnotationApplicationTests {
+    @Reference
+    private IUservice uservice;
 
     @Test
     void contextLoads() {
+        uservice.hello();
+        System.out.println(uservice.hello());
     }
 
 }
