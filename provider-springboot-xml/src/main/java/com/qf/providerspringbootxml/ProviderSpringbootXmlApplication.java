ca7dca5910f56c6f47faebfbbc71846cb5cb5a3a
guijih <18300613195@163.com>
2020-02-12 11:42:46 +0800

    dubbo-springboot

---
+++ provider-springboot-xml/src/main/java/com/qf/providerspringbootxml/ProviderSpringbootXmlApplication.java
@@ -0,0 +1,13 @@
+package com.qf.providerspringbootxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderSpringbootXmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderSpringbootXmlApplication.class, args);
+    }
+
+}
